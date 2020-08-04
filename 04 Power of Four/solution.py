60c40c61644ffc7de35064eab0a6052f3dc52c6a
Vivek0712 <vivekraja98@gmail.com>
2020-08-04 16:16:33 +0530

    Day 04

---
+++ 04 Power of Four/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        count = 0
+
+        if (n and (not(n & (n - 1)))): 
+
+            while(n > 1): 
+                n >>= 1
+                count += 1
+
+            if(count % 2 == 0): 
+                return True
+            else: 
+                return False
