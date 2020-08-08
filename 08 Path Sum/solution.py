1d3b7883bf26379213079f0616b0344820fd035f
Vivek Raja P S <vivekraja98@gmail.com>
2020-08-08 19:18:01 +0530

    Day 08

---
+++ 08 Path Sum/solution.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    cnt = 0
+    def pathSum(self, root: TreeNode, sum: int) -> int:
+        def dfs(root, start, s):
+            if not root:
+                return
+            s -= root.val 
+            if s==0:
+                self.cnt+=1
+            dfs(root.left,False, s)
+            dfs(root.right,False, s)
+            if start: 
+                dfs(root.left,True,sum)
+                dfs(root.right,True,sum)
+        
+        dfs(root, True, sum)
+        return self.cnt
